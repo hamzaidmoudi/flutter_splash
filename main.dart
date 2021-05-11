6616aa406363007e4a49d760b60dceca8af077f5
hamzaidmoudi <79338468+hamzaidmoudi@users.noreply.github.com>
2021-05-11 19:09:58 +0000

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,47 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+
+      home: AnimatedSplashScreen(
+
+        splash: Image.asset('assets\EASYHOME.png'),
+        nextScreen: MyHomePage(),
+        splashTransition: SplashTransition.decoratedBoxTransition,
+    ),
+      );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text('hamza wa3eeeeeeer'),
+      ),
+
+
+    );
+  }
+}
